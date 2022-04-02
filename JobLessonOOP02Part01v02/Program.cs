56a9a4adb638669928f42a1964f036d3eba0a52c
Safroniv <safroniv@mail.ru>
2022-04-03 00:29:19 +0300

    Чистка

--- JobLessonOOP02Part01v02/Program.cs
+++ JobLessonOOP02Part01v02/Program.cs
@@ -1,12 +1,10 @@
-﻿using System;
-
-namespace JobLessonOOP02Part01v02
+﻿namespace JobLessonOOP02Part01v02
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
